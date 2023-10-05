db8e2c4d8dcca2b8b33f22fbe328c1e565d65a4e
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-10-05 13:23:15 +0200

    add 404 error page

--- src/View/404.php
+++ src/View/404.php
@@ -4,7 +4,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="./../public/images/logo/RemindMe!.png">
+    <link rel="icon" href="super-reminder/public/images/logo/RemindMe!.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Remind Me! - 404</title>
 </head>
@@ -12,8 +12,13 @@
     <header>
         <?php require_once 'src/View/import/header.php'; ?>
     </header>
-    <main>
-        <h2>404</h2>
+    <main class="h-screen h-sreen">
+        <section class="flex items-center justify-center h-full">
+            <div class="flex flex-col items-center">
+                <h2 class="text-6xl font-bold">Erreur 404</h2>
+                <p>Cette page n'existe pas</p>
+            </div>
+        </section>
     </main>
     <footer>
         <?php require_once 'src/View/import/footer.php'; ?>
